68d75d91d87ef7f28e04be1dfb1ad1ff29b059ad
dsllt <dayanesallet@hotmail.com>
2024-11-11 13:38:14 -0300

    feat: inicializa backend

---
+++ backend/src/main/java/com/senac/curva_crescimento/CurvaCrescimentoApplication.java
@@ -0,0 +1,13 @@
+package com.senac.curva_crescimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurvaCrescimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurvaCrescimentoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/senac/curva_crescimento/CurvaCrescimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.curva_crescimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurvaCrescimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
